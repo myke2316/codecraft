e50c7727983ce05ed7d9043c9f7c943c46eefad7
myke2316 <leianmykecastillo@gmail.com>
2024-09-18 20:36:35 +0800

    Testing vps v8.4

--- client/src/features/LoginRegister/LoginForm.jsx
+++ client/src/features/LoginRegister/LoginForm.jsx
@@ -18,7 +18,7 @@ function LoginForm() {
   function handleGoogleAuth() {
     
     try {
-      window.location.href = `https://codecrafts.online/auth/google/callback`;
+      window.location.href = `https://api.codecrafts.online/auth/google/callback`;
     } catch (error) {
       toast.error(error?.data?.message || error?.error);
     }
