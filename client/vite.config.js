6ab00ee105f3c0df488657989526932cc1f03333
myke2316 <leianmykecastillo@gmail.com>
2024-09-23 22:25:22 +0800

    Certification v9.6 - Rollup error Fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,8 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["html2canvas", "jspdf"],
-  }, build: {
-    rollupOptions: {
-      external: ["html2canvas", "jspdf"],  // Externalizes these libraries if needed
-    },
   },
+
   server: {
     port: 5173,
   },
