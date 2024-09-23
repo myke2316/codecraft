3d24166e42435a624e0cf6555691c805bb20ee7e
myke2316 <leianmykecastillo@gmail.com>
2024-09-23 21:48:53 +0800

    Certification v4 - Rollup error Fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    external: ['html2canvas'], // Externalize html2canvas
+  },
   server: {
     port: 5173,
   }
