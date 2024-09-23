493e1aceabc96f3846374149172d687682a85f32
myke2316 <leianmykecastillo@gmail.com>
2024-09-23 22:16:09 +0800

    Certification v8 - Rollup error Fixing

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
+  optimizeDeps: {
+    include: ["html2canvas", "jspdf"],
+  },
+
   server: {
     port: 5173,
-  }
+  },
 });
